659d85c830815b95cb93bc1421243d2c8f125241
Soulboyfriend <96565630+Soulboyfriend@users.noreply.github.com>
2022-08-20 15:46:26 +0800

    Create BuildConfig.java

---
+++ com/andrewdev/cupheadgame/BuildConfig.java
@@ -0,0 +1,9 @@
+package com.andrewdev.cupheadgame;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.andrewdev.cupheadgame";
+    public static final String BUILD_TYPE = "debug";
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final int VERSION_CODE = 10000;
+    public static final String VERSION_NAME = "1.0.0";
+}
